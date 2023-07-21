bd0633d8ba0eb2ca53e7c47765d243ff24d92117
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-07-21 17:09:36 -0300

    Add overview links to main nav

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,4 +1,11 @@
-import { User, Map, LogOut, Newspaper } from "lucide-react";
+import {
+  User,
+  Map,
+  LogOut,
+  Newspaper,
+  GanttChartSquare,
+  CalendarRange,
+} from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
@@ -23,9 +30,9 @@ const SideBar = () => {
   return !user ? (
     <div className=""></div>
   ) : (
-    <div className="hidden flex-[1] h-screen lg:flex lg:flex-col lg:items-end lg:py-[200px] lg:pr-10">
-      <div className="rounded-md flex flex-col mx-auto w-[13.5rem] shadow-input gap-5 p-5 mt-6 bg-card border dark:shadow-none">
-        <nav className="">
+    <div className="hidden flex-[1] h-screen lg:flex lg:flex-col lg:items-center lg:py-[160px] lg:border-r lg:mr-9">
+      <div className="rounded-md flex flex-col mx-auto w-[13.5rem] gap-5">
+        <nav className="flex flex-col gap-3">
           <ul className="flex flex-col">
             <p className="text-accent uppercase pb-1 font-bold text-sm dark:text-white">
               Listas
@@ -67,31 +74,54 @@ const SideBar = () => {
               </Link>
             </li>
           </ul>
+          <ul className="flex flex-col">
+            <p className="text-accent uppercase pb-1 font-bold text-sm dark:text-white">
+              Ventas
+            </p>
+            <li className="relative flex items-center gap-2">
+              <GanttChartSquare className="absolute left-2 h-5 w-5 text-accent " />
+              <Link
+                to="/overview"
+                className="w-full pl-8 z-20 rounded-lg py-1 px-2 flex items-center gap-1 text-start bg-transparent hover:bg-hover/40 dark:hover:text-white "
+              >
+                Resumen general
+              </Link>
+            </li>
+            <li className="relative flex items-center gap-2">
+              <CalendarRange className="absolute left-2 h-5 w-5 text-accent " />
+              <Link
+                to="/monthly"
+                className="w-full pl-8 z-20 rounded-lg py-1 px-2 flex items-center gap-1 text-start bg-transparent hover:bg-hover/40 dark:hover:text-white "
+              >
+                Resumen mensual
+              </Link>
+            </li>
+          </ul>
+          <ul className="flex flex-col">
+            <p className="text-accent uppercase pb-1 font-bold text-sm dark:text-white">
+              Admin
+            </p>
+            <li className="relative flex items-center gap-2">
+              <User className="absolute left-2 h-5 w-5 text-accent " />
+              <Link
+                to="/mi-perfil"
+                className="w-full pl-8 z-20 rounded-lg py-1 px-2 flex items-center gap-1 text-start bg-transparent hover:bg-hover/40  dark:hover:text-white "
+              >
+                Perfil
+              </Link>
+            </li>
+            <li className="relative flex items-center gap-2">
+              <LogOut className="absolute left-2 h-5 w-5 text-accent " />
+              <Link
+                to="/login"
+                onClick={handleLogOut}
+                className="w-full pl-8 z-20 rounded-lg py-1 px-2 flex items-center gap-1 text-start bg-transparent hover:bg-hover/40  dark:hover:text-white "
+              >
+                Salir
+              </Link>
+            </li>
+          </ul>
         </nav>
-        <ul className="flex flex-col">
-          <p className="text-accent uppercase pb-1 font-bold text-sm dark:text-white">
-            Admin
-          </p>
-          <li className="relative flex items-center gap-2">
-            <User className="absolute left-2 h-5 w-5 text-accent " />
-            <Link
-              to="/mi-perfil"
-              className="w-full pl-8 z-20 rounded-lg py-1 px-2 flex items-center gap-1 text-start bg-transparent hover:bg-hover/40  dark:hover:text-white "
-            >
-              Perfil
-            </Link>
-          </li>
-          <li className="relative flex items-center gap-2">
-            <LogOut className="absolute left-2 h-5 w-5 text-accent " />
-            <Link
-              to="/login"
-              onClick={handleLogOut}
-              className="w-full pl-8 z-20 rounded-lg py-1 px-2 flex items-center gap-1 text-start bg-transparent hover:bg-hover/40  dark:hover:text-white "
-            >
-              Salir
-            </Link>
-          </li>
-        </ul>
       </div>
     </div>
   );
