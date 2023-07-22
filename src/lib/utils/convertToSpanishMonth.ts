50aa9ebbc751bfa290cf9f11d095a169ae97ce0d
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-07-22 14:19:08 -0300

    Add convert to spanish function

---
+++ src/lib/utils/convertToSpanishMonth.ts
@@ -0,0 +1,26 @@
+function convertToSpanishMonth(month: string) {
+    const months = {
+      January: "Enero",
+      February: "Febrero",
+      March: "Marzo",
+      April: "Abril",
+      May: "Mayo",
+      June: "Junio",
+      July: "Julio",
+      August: "Agosto",
+      September: "Septiembre",
+      October: "Octubre",
+      November: "Noviembre",
+      December: "Diciembre",
+    };
+
+    // Convert the input to title case (e.g., "january" to "January")
+    const formattedMonth =
+      month.charAt(0).toUpperCase() + month.slice(1).toLowerCase();
+
+    return months[formattedMonth] || "Invalid Month";
+  }
+
+ export {
+    convertToSpanishMonth,
+ }
