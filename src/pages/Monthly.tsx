74b05625122e95976117b8cd181a91de8a87b6cf
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-07-22 14:18:39 -0300

    Add monthly resume page

---
+++ src/pages/Monthly.tsx
@@ -0,0 +1,117 @@
+import { useMemo, useState } from "react";
+import { Box } from "@mui/material";
+import SectionTitle from "@/components/SectionTitle";
+import useFetch from "@/hooks/useFetch";
+import { ResponsiveLine } from "@nivo/line";
+import { convertToSpanishMonth } from "@/lib/utils/ConvertToSpanishMonth";
+
+const Monthly = () => {
+  const { data, loading } = useFetch(
+    "https://fabebus-api-example.onrender.com/api/sales"
+  );
+  const [formattedData] = useMemo(() => {
+    if (!data || !data.monthlyData) return [[], []]; // Add a check for data and monthlyData existence
+
+    const { monthlyData } = data;
+    console.log(monthlyData);
+    const totalSalesLine = {
+      id: "Ingresos",
+      color: "hsl(347, 70%, 50%)",
+      data: [],
+    };
+
+    Object.values(monthlyData).forEach(({ month, totalSales }) => {
+      totalSalesLine.data = [
+        ...totalSalesLine.data,
+        { x: convertToSpanishMonth(month), y: totalSales },
+      ];
+    });
+
+    const formattedData = [totalSalesLine];
+    return [formattedData];
+  }, [data]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <Box m="1.5rem 0">
+      <SectionTitle>Ventas mensuales</SectionTitle>
+      <p className="text-card-foreground">Gráfico de ingresos mensuales.</p>
+      <Box height="75vh">
+        {data ? (
+          <ResponsiveLine
+            data={formattedData}
+            margin={{ top: 50, right: 50, bottom: 70, left: 60 }}
+            colors={{ datum: "color" }}
+            xScale={{ type: "point" }}
+            yScale={{
+              type: "linear",
+              min: "auto",
+              max: "auto",
+              stacked: false,
+              reverse: false,
+            }}
+            yFormat=" >-.2f"
+            curve="catmullRom"
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+              orient: "bottom",
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 90,
+              legend: "Mes",
+              legendOffset: 60,
+              legendPosition: "middle",
+            }}
+            axisLeft={{
+              orient: "left",
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "Total",
+              legendOffset: -50,
+              legendPosition: "middle",
+            }}
+            enableGridX={false}
+            enableGridY={false}
+            pointSize={10}
+            pointColor={{ theme: "background" }}
+            pointBorderWidth={2}
+            pointBorderColor={{ from: "serieColor" }}
+            pointLabelYOffset={-12}
+            useMesh={true}
+            legends={[
+              {
+                anchor: "top-right",
+                direction: "column",
+                justify: false,
+                translateX: 50,
+                translateY: 0,
+                itemsSpacing: 0,
+                itemDirection: "left-to-right",
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: "circle",
+                symbolBorderColor: "rgba(0, 0, 0, .5)",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemBackground: "rgba(0, 0, 0, .03)",
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+        ) : (
+          <>Loading...</>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default Monthly;
