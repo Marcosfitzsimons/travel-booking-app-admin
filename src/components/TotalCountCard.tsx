e37dff14e19458bef12a61428a7c548b983f1cfd
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-07-27 14:25:40 -0300

    Add total count card component

---
+++ src/components/TotalCountCard.tsx
@@ -0,0 +1,26 @@
+type TotalCountCardProps = {
+  icon: any;
+  title: string;
+  value: any;
+};
+
+const TotalCountCard = ({ icon, title, value }: TotalCountCardProps) => {
+  return (
+    <article className="flex items-center gap-4 bg-card py-4 px-8 border shadow-input rounded-lg dark:shadow-none">
+      <div className="">{icon}</div>
+      <div className="flex flex-col">
+        <h4 className="text-card-foreground">{title}</h4>
+        <p className="text-lg font-bold flex items-center gap-1">
+          <span
+            className={`animate-pulse w-3 h-3 rounded-full ${
+              value > 0 ? "bg-green-500" : "bg-red-600"
+            }`}
+          />
+          {value}
+        </p>
+      </div>
+    </article>
+  );
+};
+
+export default TotalCountCard;
