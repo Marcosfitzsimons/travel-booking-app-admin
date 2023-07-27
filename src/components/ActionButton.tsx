2f6d1683eca5b7963feb1e6949b523a76754ba81
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-07-27 14:28:33 -0300

    Update action buttons

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -13,7 +13,7 @@ const ActionButton = ({ linkTo, icon, text }: ActionButtonProps) => {
       <div className="relative after:absolute after:pointer-events-none after:inset-px after:rounded-[7px] after:shadow-highlight after:shadow-slate-100/20 dark:after:shadow-highlight dark:after:shadow-slate-100/30 after:transition focus-within:after:shadow-slate-100 dark:focus-within:after:shadow-slate-100">
         <Button
           className="h-[32px] px-3.5 pl-[35px] relative bg-teal-800/60 text-white shadow-input hover:text-white
-dark:text-slate-100 dark:bg-teal-700/60 dark:hover:text-white dark:shadow-none"
+dark:text-slate-100 dark:bg-teal-700/60 md:text-base dark:hover:text-white dark:shadow-none"
         >
           <Link to={linkTo}>
             {icon}
