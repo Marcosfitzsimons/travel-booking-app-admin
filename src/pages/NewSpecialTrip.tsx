2d5a05be5346c1075980d5fe949bd621024fb1fa
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-07-27 17:58:26 -0300

    Update new special trip page

--- src/pages/NewSpecialTrip.tsx
+++ src/pages/NewSpecialTrip.tsx
@@ -1,6 +1,5 @@
 import BackButton from "../components/BackButton";
 import NewSpecialTripForm from "../components/NewSpecialTripForm";
-import NewTripForm from "../components/NewTripForm";
 import SectionTitle from "../components/SectionTitle";
 
 interface InputValidation {
@@ -39,11 +38,21 @@ type NewTripProps = {
 const NewSpecialTrip = ({ inputs, title }: NewTripProps) => {
   return (
     <section className="flex flex-col gap-5">
-      <SectionTitle>{title}</SectionTitle>
-      <div className="self-start mb-2">
+      <div className="self-start">
         <BackButton linkTo="/special-trips" />
       </div>
-      <div className="p-5 rounded-md bg-card w-full max-w-md self-center border lg:max-w-3xl lg:self-start">
+      <SectionTitle>{title}</SectionTitle>
+      <div className="relative px-5 py-10 rounded-lg bg-card w-full max-w-md self-center border lg:max-w-3xl lg:self-start">
+        <div className="absolute top-[0.75rem] left-2.5 sm:left-4 flex flex-col gap-[3px] transition-transform ">
+          <span className="w-8 h-[4px] bg-red-700 rounded-full " />
+          <span className="w-4 h-[4px] bg-red-700 rounded-full " />
+          <span className="w-2 h-[4px] bg-red-700 rounded-full " />
+        </div>
+        <div className="absolute bottom-[0.75rem] right-2.5 sm:left-4 flex flex-col rotate-180 gap-[3px] transition-transform ">
+          <span className="w-8 h-[4px] bg-red-700 rounded-full " />
+          <span className="w-4 h-[4px] bg-red-700 rounded-full " />
+          <span className="w-2 h-[4px] bg-red-700 rounded-full " />
+        </div>
         <NewSpecialTripForm inputs={inputs} />
       </div>
     </section>
