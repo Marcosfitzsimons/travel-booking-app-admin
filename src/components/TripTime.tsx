c90c366e81e3f1d7f3e0c0af4d39877ddecc17fc
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-09-13 17:52:33 -0300

    Add tripTime component

---
+++ src/components/TripTime.tsx
@@ -0,0 +1,16 @@
+import { Clock } from "lucide-react";
+
+type TripTimeProps = {
+  children: React.ReactNode;
+};
+
+const TripTime = ({ children }: TripTimeProps) => {
+  return (
+    <p className="flex items-center text-sm px-2 select-none gap-1 rounded-lg bg-indigo-100 dark:bg-indigo-300/20">
+      <Clock className="h-4 w-4 shrink-0" />
+      {children}
+    </p>
+  );
+};
+
+export default TripTime;
