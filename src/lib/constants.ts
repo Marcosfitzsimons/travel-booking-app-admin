b05958ecee1cf642f2e2ba7442a5beff24c8aa42
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-09-22 21:19:05 -0300

    Add constants to utils folder

---
+++ src/lib/constants.ts
@@ -0,0 +1,19 @@
+export const dayOfWeekToNumber = {
+    sunday: 0,
+    monday: 1,
+    tuesday: 2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6,
+  };
+  
+  export const dayOfWeekTranslations: Record<string, string> = {
+    sunday: "Domingo",
+    monday: "Lunes",
+    tuesday: "Martes",
+    wednesday: "Miércoles",
+    thursday: "Jueves",
+    friday: "Viernes",
+    saturday: "Sábado",
+  };
