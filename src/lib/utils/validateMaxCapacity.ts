99f2eddbdd70d6d5ab2ed3ae26cfe6e65b607338
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-02-13 14:44:10 -0300

    Change max capacity field validation

--- src/lib/utils/validateMaxCapacity.ts
+++ src/lib/utils/validateMaxCapacity.ts
@@ -2,6 +2,10 @@
 export const validateMaxCapacity = (value: string, passengersLength: number) => {
     const maxCapacity = parseInt(value, 10);
 
+    if(maxCapacity === 0) {
+      return "La capacidad máxima no puede ser 0";
+    }
+
     if (isNaN(maxCapacity)) {
       return "Por favor, ingresa un número válido";
     }
