6866113446b343a9b51e926247ed5e0a4899bbcb
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-02-15 09:49:04 -0300

    Change button name

---
+++ src/components/TrashButton.tsx
@@ -0,0 +1,15 @@
+type TrashButtonProps = {
+  icon: any;
+  text: string;
+};
+
+const TrashButton = ({ text, icon }: TrashButtonProps) => {
+  return (
+    <span className="pl-[21px] rounded-md text-[#b4343a] font-semibold transition-colors hover:text-red-300">
+      {icon}
+      {text}
+    </span>
+  );
+};
+
+export default TrashButton;
